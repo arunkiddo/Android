50fb5dcc174808a54e05227dcd8852a04669046d
arunkiddo <karunganessh28@gmail.com>
2018-02-13 18:23:15 +0530

    Android App - ChatBot Application
    In this i have Dev my ChatBot in Android.Check it out.It might be useful for You guys

---
+++ ChatBotFinal/app/src/main/java/com/example/arun/chatbotfinal/Models/ChatModel.java
@@ -0,0 +1,34 @@
+package com.example.arun.chatbotfinal.Models;
+
+/**
+ * Created by lokesh on 9/15/2017.
+ */
+
+public class ChatModel {
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+
+    public ChatModel() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+}
