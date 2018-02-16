3e51e1c3d0038c53bb0ed0f0331ad72516cd3e37
arunkiddo <karunganessh28@gmail.com>
2018-02-16 15:20:10 +0530

    cHAT bot Try
    Its samll try

---
+++ Chtas/app/src/main/java/com/example/monishsuresh/chtas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.monishsuresh.chtas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
